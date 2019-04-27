5b2407f5b130c5b06487b9efb0bb62d9bff20126
santosh0305 <2mymail.santosh@gmail.com>
2019-04-27 22:53:56 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { of } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+import { map, tap, take } from 'rxjs/operators';
 
 // Of
 const source = of('World').pipe(
@@ -9,13 +9,17 @@ source.subscribe(x => console.log(x));
 
 // Reduce Operator with array
 let numbers = [1,2,3];
-const number = of(...numbers).pipe(
-  map(number => `number : ${number}!`)
+const result = of(...numbers).pipe(
+  map(result => `result : ${result}!`)
 );
-number.subscribe(x => console.log(x));
+result.subscribe(x => console.log(x));
 
-// Reduce on Object 
-let assetTypes =[{assetTypdId:1,assetType:'Type1'},
-                {assetTypdId:2,assetType:'Type2'}];
-const assets = of(...assetTypes);
-assets.subscribe(x=>console.log(x));
+// Take : this takes / prints only 1 item in array
+// tap : it will give per item details
+let assetTypes =[{assetTypedId:1,assetType:'Type1'},{assetTypedId:2,assetType:'Type2'},{assetTypedId:3,assetType:'Type3'},{assetTypedId:4,assetType:'Type4'},{assetTypedId:5,assetType:'Type5'}];
+const assets = of(...assetTypes).pipe(
+      take(2),
+      tap(x => console.log('Tap info',x)),
+      map(assets => console.log('Asset Details : ',assets.assetTypedId,assets.assetType))
+  );
+assets.subscribe(x => console.log('output : ',x));
