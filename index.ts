db0aedbd6f65244de3e38a96605f1d58565c1ccd
santosh0305 <2mymail.santosh@gmail.com>
2019-04-28 16:12:33 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -7,8 +7,13 @@ import {
   switchMap, 
   debounceTime, 
   distinctUntilChanged, 
-  combineLatest } 
+  combineLatest,
+  mergeMap,
+  catchError
+} 
 from 'rxjs/operators';
+import { interval, fromEvent, merge, empty } from 'rxjs';
+import {  scan, takeWhile, startWith, mapTo } from 'rxjs/operators';
 
 // Of
 const source = of('World').pipe(
@@ -176,3 +181,27 @@ source
     // take action
   });
 
+// Switch Map
+// RxJS v6+
+
+
+const COUNTDOWN_SECONDS = 10;
+
+// elem refs
+const remainingLabel = document.getElementById('remaining');
+const pauseButton = document.getElementById('pause');
+const resumeButton = document.getElementById('resume');
+
+// streams
+const interval$ = interval(1000).pipe(mapTo(-1));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+const resume$ = fromEvent(resumeButton, 'click').pipe(mapTo(true));
+
+const timer$ = merge(pause$, resume$)
+  .pipe(
+    startWith(true),
+    switchMap(val => (val ? interval$ : empty())),
+    scan((acc, curr) => (curr ? curr + acc : acc), COUNTDOWN_SECONDS),
+    takeWhile(v => v >= 0)
+  )
+  .subscribe((val: any) => (remainingLabel.innerHTML = val));
